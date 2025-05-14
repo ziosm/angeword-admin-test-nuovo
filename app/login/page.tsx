7ececba3eab0d5f5323f7dfffd8168843254f274
ziosm <scalia.beniamino@gmail.com>
2025-05-14 03:06:33 +0200

    Add files via upload

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Pagina di Login Minimale</h1>
+      <p>Questa è la pagina di login del sito di test.</p>
+      <a href="/">Torna alla Homepage</a>
+    </div>
+  );
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Benvenuto nella Homepage Minimale</h1>
+      <p>Questa è la pagina principale del sito di test.</p>
+      <a href="/login">Vai alla pagina di Login</a>
+    </div>
+  );
+}
+
